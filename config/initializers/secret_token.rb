fa6bb15e6d7453e56ddfe24858aa7d9d0357e398
kryss88ltj <christine.stauffer@gmail.com>
2014-03-20 17:42:35 -0400

    first commit and updated README.md

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Stabloccit::Application.config.secret_key_base = '05053a63f7717dedbf130dbd4e12c4ea4179eb72d7dab7d34e96e09dff9e0110e9b139541e1292d004db3b2c40276f0eb19e764ec045d6339045847763f192a1'
