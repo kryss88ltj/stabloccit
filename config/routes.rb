51ae547a7b1f2b1c9f77697617f019a10dd767a6
kryss88ltj <christine.stauffer@gmail.com>
2014-03-21 13:55:05 -0400

    Created Post resource and updated index and show views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Stabloccit::Application.routes.draw do
-  get "welcome/index"
-  get "welcome/about"
+  # get "posts/index"
+  # get "posts/show"
+  # get "posts/new"
+  # get "posts/edit"
+  # get "welcome/index"
+  # get "welcome/about"
+  # this above is what existed before we changed it to the below
+
+  resources :posts
+
+  match "about", to: 'welcome#about', via: :get
+
 
   root to: 'welcome#index'
 
