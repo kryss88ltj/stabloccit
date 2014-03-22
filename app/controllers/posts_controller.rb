59d6aff373f2534c68a392ccb84856ce44ee7b15
kryss88ltj <christine.stauffer@gmail.com>
2014-03-22 15:41:27 -0400

    associated users and posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,8 +12,7 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.new(params.require(:post).permit(:title, :body))
-
+    @post = current_user.posts.build(params.require(:post).permit(:title, :body))
     if @post.save
       flash[:notice] = "Post was saved."
       redirect_to @post #redirect_to extracts the id from @post and knows to use it in the path
