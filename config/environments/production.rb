699f872428d1d05e357236d7ee1e1a62c05be536
kryss88ltj <christine.stauffer@gmail.com>
2014-03-23 17:24:38 -0400

    configured email in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,8 @@ Stabloccit::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  # http://stabloccit.herokuapp.com
+  config.action_mailer.default_url_options = { host: 'stabloccit.herokuapp.com' }
+
 end
