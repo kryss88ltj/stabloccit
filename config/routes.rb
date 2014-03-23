5f5960cafc044a2f812aa72c516e448a4599a031
kryss88ltj <christine.stauffer@gmail.com>
2014-03-23 16:08:23 -0400

    associated and nested topics and posts.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Stabloccit::Application.routes.draw do
+
   devise_for :users
   # get "posts/index"
   # get "posts/show"
@@ -8,7 +9,9 @@ Stabloccit::Application.routes.draw do
   # get "welcome/about"
   # this above is what existed before we changed it to the below
 
-  resources :posts
+  resources :topics do
+    resources :posts, except: [:index]
+  end
 
   match "about", to: 'welcome#about', via: :get
 
